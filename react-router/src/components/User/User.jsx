d126b009b08472032c328305efcb74e71b7eaef0
Smarika-tech <smarikapokhrel96@gmail.com>
2025-12-26 16:04:29 +0545

    used useParams hook to display user id

---
+++ react-router/src/components/User/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+const User = () => {
+    const {userid}= useParams()
+  return (
+    <div className='text-center text-2xl bg-red-200 p-4'>
+        User: {userid}
+    </div>
+  )
+}
+
+export default User
