db97e7cc0167838cfb4dec1586b5f6e12d7bd09f
Smarika-tech <smarikapokhrel96@gmail.com>
2025-12-26 20:07:23 +0545

    added images and handled using props

--- react-router/src/components/About/About.jsx
+++ react-router/src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { about } from '../../constant/image'
 
 const About = () => {
   return (
@@ -6,8 +7,8 @@ const About = () => {
             <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                     <div className="md:5/12 lg:w-5/12">
-                        <img
-                            src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                        <img className='ml-20'
+                            src={about}
                             alt="image"
                         />
                     </div>
---
+++ react-router/src/constant/image.js
@@ -0,0 +1,4 @@
+import img1 from '../assets/img1.png'
+import img2 from '../assets/img2.png'
+import about from '../assets/img3.png'
+export {img1, img2, about}
