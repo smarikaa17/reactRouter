f7f6ff008ef19ed90c5a7982a26c1bee4b188084
Smarika-tech <smarikapokhrel96@gmail.com>
2025-12-26 14:28:04 +0545

    Set up createBrowserRouter for client-side routing , added Layout component as root wrapper , Implemented nested routes for Home, About, and Contact pages and integrated RouterProvider at application root passing router props

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './layout.jsx'
+import About from './components/About/About.jsx'
+import Home from './components/Home/Home.jsx'
+import Contact from './components/Contact/Contact.jsx'
+const router=createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children:
+    [
+    {
+      path: '',
+      element: <Home/>
+    },
+    {
+      path:'/about',
+      element:<About/>
+    },
+    {
+      path:'/contact',
+      element:<Contact/>
+    }
+  ]
+  }
+  ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
