a49dc7459e1fca444f2ace10c1050a68137c97f5
Smarika-tech <smarikapokhrel96@gmail.com>
2025-12-26 16:01:06 +0545

    implemented nested route for github and for handling parameter

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -7,12 +7,17 @@ import Layout from './layout.jsx'
 import About from './components/About/About.jsx'
 import Home from './components/Home/Home.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
+
 const router=createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
       <Route path='' element={<Home/>}/>
       <Route path='about' element={<About/>}/>
       <Route path='contact' element={<Contact/>}/>
+      <Route path='user/:userid' element={<User/>}/>
+       <Route path='github' element={<Github/>}/>
     </Route>
   )
 )
