a9ed404b3f3152c3423206371ac6decf8de686fc
Smarika-tech <smarikapokhrel96@gmail.com>
2025-12-26 14:37:59 +0545

    add nested routing using createRoutesFromElements

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -2,32 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
 import Layout from './layout.jsx'
 import About from './components/About/About.jsx'
 import Home from './components/Home/Home.jsx'
 import Contact from './components/Contact/Contact.jsx'
-const router=createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout/>,
-    children:
-    [
-    {
-      path: '',
-      element: <Home/>
-    },
-    {
-      path:'/about',
-      element:<About/>
-    },
-    {
-      path:'/contact',
-      element:<Contact/>
-    }
-  ]
-  }
-  ])
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
