e7d75d8a7a189b86972c5b9353064a6930ec6234
Smarika-tech <smarikapokhrel96@gmail.com>
2025-12-29 22:37:40 +0545

    add global light/dark mode with context and toggle

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -1,12 +1,39 @@
 import './App.css'
+import Card from './Components/Card'
+import ThemeButton from './Components/ThemeButton'
+import { ThemeProvider } from './Context/Theme'
+import { useState, useEffect } from 'react'
 
 function App() {
- 
-  return (
+   const [themeMode, setThemeMode] = useState("Light")
+   const lightTheme = () =>{
+      setThemeMode("Light")
+   }
+
+   const darkTheme = () =>{
+      setThemeMode("dark")
+   }
      
-  <div>
-  ThemeChanger
-  </div>
+   useEffect(() => {
+     document.querySelector('html').classList.remove("light","dark")
+     document.querySelector('html').classList.add(themeMode)
+   }, [themeMode])
+   
+
+  return (
+     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+   <div className="flex flex-wrap  items-center">
+                <div className="w-full">
+                    <div className=" max-w-sm mx-auto flex justify-end mb-4">
+                      <ThemeButton/>  
+                    </div>
+
+                    <div className=" max-w-sm mx-auto">
+                       <Card/>
+                    </div>
+                </div>
+            </div>
+     </ThemeProvider>       
   )
 }
 
